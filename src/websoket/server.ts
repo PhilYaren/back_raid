2737fe8c699842012ebae1e09313d34dbc1b58ef
Yaroslav <philyaren@yandex.ru>
2023-04-12 12:51:26 +0400

    installed socket.io

--- src/websoket/server.ts
+++ src/websoket/server.ts
@@ -3,12 +3,16 @@ import Websoket from 'ws';
 const Server = Websoket.WebSocketServer;
 
 const wss = new Server({
-  clientTracking: false,
+  clientTracking: true,
   noServer: true,
 });
 
 wss.on('connection', (ws: Websoket) => {
   console.log('connected');
+  ws.on('open', () => {
+    console.log('opened');
+    ws.send('hello');
+  });
 
   ws.on('message', (message: string) => {
     const newMessage = JSON.parse(message);
